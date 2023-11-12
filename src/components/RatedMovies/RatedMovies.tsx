269eb6d2742b62e363e193460c94473fa7b8db96
Andrey <andrey@MacBook-Air-Andrey.local>
2023-11-12 21:50:19 +0300

    ul

--- src/components/RatedMovies/RatedMovies.tsx
+++ src/components/RatedMovies/RatedMovies.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { IMoviesRated } from '../../types/types';
 import MovieItemRated from '../MovieItemRated/MovieItemRated';
+import s from './RatedMovies.module.scss';
 
 interface RatedMoviesProps {
   dataRatedMovies: IMoviesRated[];
@@ -8,12 +9,12 @@ interface RatedMoviesProps {
 
 const RatedMovies: React.FC<RatedMoviesProps> = ({ dataRatedMovies }) => {
   return (
-    <div>
+    <ul className={s.movieList}>
       {dataRatedMovies.map((item) => {
         const { ...itemProps } = item;
         return <MovieItemRated key={item.id} {...itemProps} />;
       })}
-    </div>
+    </ul>
   );
 };
 
